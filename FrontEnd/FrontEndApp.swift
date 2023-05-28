183d6d96032de1fa5af910e11062e9040cba18af
AjitKumar-1 <ajitkumarxc8@gmail.com>
2023-05-28 12:56:15 +0100

    Initial Commit

---
+++ FrontEnd/FrontEndApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrontEndApp.swift
+//  FrontEnd
+//
+//  Created by Ajit Kumar on 28/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct FrontEndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
