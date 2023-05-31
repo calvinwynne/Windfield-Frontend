13497e4bda738102cdd98fff261b577225b3c823
Ajit Kumar <ajitkumarxc8@gmail.com>
2023-05-31 17:41:15 +0100

    Created simple progress bar that increments progress every second

--- FrontEnd/ContentView.swift
+++ FrontEnd/ContentView.swift
@@ -9,16 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var progress = 0
+    @State private var progress: Double = 0
+    
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
-    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            ProgressView(value: 0, total: 100)
+            ProgressView(value: progress, total: 100)
+                .onReceive(timer) { _ in
+                    progress = progress < 100 ? progress + 1 : 100
+                }
         }
         .padding()
     }
